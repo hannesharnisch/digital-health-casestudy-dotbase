af2ddac721b7a4737f9a4c5985b9076a2e62f12b
hgraeser <henry.graeser@semdatex.com>
2024-03-19 12:18:03 +0100

    Changed paths of sample_data

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import {demonstrateElAPSKPI} from "./EncounterLengthAndPatientSatisfaction/Encou
 
 // Patient satisfaction
 console.log("Patient Satisfaction KPI:")
-console.log(showObject(patientSatisfactionDemonstration('/Users/henry/Desktop/digital-health-casestudy-dotbase/src/PatientSatisfaction/FHIR_data/samplePatientSatisfactionBundle.fhir.json')))
+console.log(showObject(patientSatisfactionDemonstration('./sample_data/PatientSatisfacton/samplePatientSatisfactionBundle.fhir.json')))
 
-const revenues = demonstrateRevenue("/Users/henry/Desktop/digital-health-casestudy-dotbase/src/Revenue/exampleRevenueAPIResponse.json")
+const revenues = demonstrateRevenue("./sample_data/Revenue/exampleRevenueAPIResponse.json")
 console.log("\n")
 console.log("Total Revenue")
 console.log(showObject(revenues.total))
@@ -21,12 +21,12 @@ revenues.departments.forEach((elem) => {
 
 console.log("\n")
 console.log("Average Length Of Stay")
-console.log(showObject(demonstrateAverageLengthOfStayKPI("/Users/henry/Desktop/digital-health-casestudy-dotbase/src/AverageLengthOfStay/encounters.json")))
+console.log(showObject(demonstrateAverageLengthOfStayKPI("./sample_data/AverageLengthOfStay/encounters.json")))
 
 console.log("\n")
 console.log("Average Length Of Encounter")
-console.log(showObject(demonstrateEncounterLength("/Users/henry/Desktop/digital-health-casestudy-dotbase/src/EncounterLength/encounters.json")))
+console.log(showObject(demonstrateEncounterLength("./sample_data/EncounterLength/encounters.json")))
 
 console.log("\n")
 console.log("Patient satisfactiona and average length of encounter ratio")
-console.log(showObject(demonstrateElAPSKPI("/Users/henry/Desktop/digital-health-casestudy-dotbase/src/EncounterLength/encounters.json", '/Users/henry/Desktop/digital-health-casestudy-dotbase/src/PatientSatisfaction/FHIR_data/samplePatientSatisfactionBundle.fhir.json')))
+console.log(showObject(demonstrateElAPSKPI("./sample_data/EncounterLength/encounters.json", './sample_data/PatientSatisfacton/samplePatientSatisfactionBundle.fhir.json')))
