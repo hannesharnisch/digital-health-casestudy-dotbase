b43e8876c630f60568cbff906fcf9f6c0bd4e961
hgraeser <henry.graeser@semdatex.com>
2024-03-21 11:53:10 +0100

    Added Revenue KPI

--- src/aggregators/index.ts
+++ src/aggregators/index.ts
@@ -3,10 +3,12 @@ import EncounterLength from './EncounterLengthAggregator';
 import EncounterLengthAndPatientSatisfaction from './EncounterLengthAndPatientSatisfactionAggregator';
 import KPIAggregator from './KPIAggregator';
 import PatientSatisfaction from './PatientSatisfactionAggregator';
+import RevenueAggregator from "./RevenueAggregator";
 
 export default [
   AverageLengthOfStay,
   EncounterLength,
   PatientSatisfaction,
   EncounterLengthAndPatientSatisfaction,
+  RevenueAggregator,
 ] as (new () => KPIAggregator)[];
